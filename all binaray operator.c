435c1de860e8e226e5ccbee64f6202352d063bbd
Nitin-Mewada <163722189+Nitin-Mewada@users.noreply.github.com>
2024-03-21 11:50:48 +0530

    Add files via upload

---
+++ all binaray operator.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,sum,sub,mul,div,modulus;
+	printf("Enter the two number\n");
+	scanf("%d%d",&a,&b);
+	sum=a+b;
+	printf("addition of two number=%d\n",sum);
+	sum=a-b;
+	print("substraction of two number=%d\n",sub);
+	sum=a*b;
+	printf("multiplication of two number=%d\n",mul);
+	sum=a/b;
+	printf("division of two number=%d\n",div);
+	sum=a%b;
+	printf("modulus of two number=%d\n",modulus);
+	
+//	sum=a+b;
+	return 0;
+}
