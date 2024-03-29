321d2b8168098b7a263b2d9fcf65559ca015fc59
Nitin-Mewada <163722189+Nitin-Mewada@users.noreply.github.com>
2024-03-29 12:27:50 +0530

    Add files via upload

---
+++ armstrong.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int i,sum=0,x;
+	printf("ENTER NUMBER TO CHECK ARMSTRON=");
+	scanf("%d",&i);
+	x=i;
+	while(i>0)
+	{
+		sum=sum+(i%10)*(i%10)*(i%10);
+		i=i/10;
+	}
+	if(sum==x)
+	printf("ARMSTRONG NUMBER");
+	else
+	printf("NOT ARMSTRONG NUMBER");
+	return 0;
+}
