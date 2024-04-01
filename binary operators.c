0f442a66e8d74d4b8d2f885881e22925706faa57
Nitin-Mewada <163722189+Nitin-Mewada@users.noreply.github.com>
2024-04-01 09:06:09 +0530

    Add files via upload

---
+++ binary operators.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,sum,sub,mul,division,modulus;
+	printf("Enter the two number\n");
+	scanf("%d%d",&a,&b);
+	sum=a+b;
+	printf("addition of two number=%d\n",sum);
+	sub=a-b;
+	printf("substraction of two number=%d\n",sub);
+	mul=a*b;
+	printf("multiplication of two number=%d\n",mul);
+	division=a/b;
+	printf("division of two number=%d\n",division);
+	modulus=a%b;
+	printf("modulus of two number=%d\n",modulus);
+	
+	/*
+		
+	
+	float c,f;
+	printf("Enter the temperature in celcius:\n");
+	scanf("%f\n",&c);
+	f=(c*9/5)+32;
+	printf(" temperature in fahrenheit=%f",f);
+	return 0;
+}*/
+return 0;
+}
