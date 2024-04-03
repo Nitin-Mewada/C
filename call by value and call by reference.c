0e161b041976acf07458d453d9e723c0a83750a2
Nitin-Mewada <163722189+Nitin-Mewada@users.noreply.github.com>
2024-04-03 21:49:17 +0530

    Add files via upload

---
+++ call by value and call by reference.c
@@ -0,0 +1,39 @@
+/*
+// call by value
+#include<stdio.h>
+void change(int);
+void main()
+{
+	int a;
+	printf("ENTER A NUMBER=");
+	scanf("%d",&a);
+	printf("\n VALUE OF A BEFORE FUNCTION CALL=%d",a);
+	change(a);
+	printf("\n VALUE OF A AFTER FUNCTION CALL=%d",a);
+}
+change(int a)
+{
+	a=a+10;
+	printf("\nVALUE OF A=%d",a);
+	
+} */
+
+// call by reference
+#include<stdio.h>
+void change(int *);
+void main()
+{
+	int a,*p;
+	printf("ENTER A NUMBER=");
+	scanf("%d",&a);
+	printf("\n VALUE OF A BEFORE FUNCTION CALL=%d",a);
+	p=&a;
+	change(p);
+	printf("\n VALUE OF A AFTER FUNCTION CALL=%d",a);
+}
+change(int *p)
+{
+	*p=*p+10;
+	printf("\nVALUE OF p=%d",*p);
+	
+}
