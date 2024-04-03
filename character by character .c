76c79e04875f95db3c9c6e71f54bf26c0edc0f4d
Nitin-Mewada <163722189+Nitin-Mewada@users.noreply.github.com>
2024-04-03 21:49:46 +0530

    Add files via upload

---
+++ character by character .c
@@ -0,0 +1,17 @@
+ // WAP ACCEPT A STRING AND PRINT to CHARACTER BY CHARACTER
+ #include<stdio.h>
+int main()
+{
+	char a[30];
+	int i;
+	printf("ENTER YOUR NAME:");
+	fgets(a,sizeof(a),stdin);
+	i=0;
+	while(a[i]!='\0')
+	{
+		
+		printf("%c\n",a[i]);
+		i++;
+	}
+	return 0;
+}  
