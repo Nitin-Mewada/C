e98ef17dfa75a091776462bf8404034693901e61
Nitin-Mewada <163722189+Nitin-Mewada@users.noreply.github.com>
2024-04-07 22:40:06 +0530

    Add files via upload

---
+++ string.c
@@ -0,0 +1,14 @@
+/*How to read a line of text?
+You can use the fgets() function to read a line of string. And, you can use puts() to display the string.
+
+Example 2: fgets() and puts() */
+#include <stdio.h>
+int main()
+{
+    char name[30];
+    printf("Enter name: ");
+    fgets(name, sizeof(name), stdin);  // read string
+    printf("Name: ");
+    puts(name);    // display string
+    return 0;
+} 
