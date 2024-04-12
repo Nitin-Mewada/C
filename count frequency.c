2943fd65b96381eff620098b1fe7b1d9005ce90c
Nitin-Mewada <163722189+Nitin-Mewada@users.noreply.github.com>
2024-04-12 22:54:42 +0530

    Add files via upload

---
+++ count frequency.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int a[10],i,freq=0,key;
+	for(i=0;i<10;i++)
+	{
+		printf("ENTER THE NUMBER=");
+		scanf("%d",&a[i]);
+	}	
+	printf("ENTER NUMBER TO FIND FREQUENCY:=");
+	scanf("%d",&key);
+	for(i=0;i<10;i++)
+	if(a[i]==key)
+	freq++;
+	printf("FREQUENCY OF %d is %d",key,freq);
+	return 0;
+}
