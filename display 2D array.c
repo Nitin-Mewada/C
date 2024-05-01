485b2f7638c4b173f961355721988f16d9af66b3
Nitin-Mewada <163722189+Nitin-Mewada@users.noreply.github.com>
2024-05-01 22:52:45 +0530

    Add files via upload

---
+++ display 2D array.c
@@ -0,0 +1,21 @@
+// ACCEPT AND DISPLAY A 3*3 MATRIX
+#include<stdio.h>
+int main()
+{
+	int a[3][3],i,j;
+	for	(i=0;i<3;i++)
+	for	(j=0;j<3;j++)
+	{
+		printf("\nENTER VALUE OF 2D ARRAY:");
+		scanf("%d",&a[i][j]);
+	}
+	printf("\n2D ARRAY ELEMENTS ARE:");
+	for	(i=0;i<3;i++)
+	{
+		printf("\n");
+		for	(j=0;j<3;j++)
+		printf("%d\t",a[i][j]);
+	}
+	return 0;
+
+}
