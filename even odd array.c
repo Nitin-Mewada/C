0ed69ef9d98a6cc8ba3773045bcccfb784df0980
Nitin-Mewada <163722189+Nitin-Mewada@users.noreply.github.com>
2024-05-05 13:38:18 +0530

    Add files via upload

---
+++ even odd array.c
@@ -0,0 +1,19 @@
+ #include<stdio.h>
+int main()
+{
+	int a[5],i,even=0,odd=0;
+	for(i=0;i<5;i++)
+	{
+		printf("ENTER THE NUMBER");
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<5;i++)
+	{
+		if(a[i]%2==0)
+		even++;
+		else
+		odd++;       
+	}
+	printf("\nTOTAL EVEN NUMBER=%d and TOTAL ODD NUMBER=%d",even,odd);
+	return 0;
+}
