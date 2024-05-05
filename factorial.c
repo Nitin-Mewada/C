bbaae21720f26fab0b1d32c1f6b21e61582e1061
Nitin-Mewada <163722189+Nitin-Mewada@users.noreply.github.com>
2024-05-05 13:39:22 +0530

    Add files via upload

---
+++ factorial.c
@@ -0,0 +1,18 @@
+        //WAP TO FIND FACTORIAL OF GIVEN NUMBER
+
+
+#include<stdio.h>
+int main()
+{
+    int i,fact=1;
+    printf("Enter the number\n");
+    scanf("%d",&i);
+    while(i>=1)
+    {
+        fact=fact*i;
+        i--;
+    }
+    printf("factorial=%d\n",fact);
+    return 0;
+    
+}
