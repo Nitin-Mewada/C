2ed70f9739bcaf7930efe04c38d6658e2d623c34
Nitin-Mewada <163722189+Nitin-Mewada@users.noreply.github.com>
2024-05-14 08:51:58 +0530

    Add files via upload

---
+++ for loop1.c
@@ -0,0 +1,31 @@
+/*#include<stdio.h>
+int main()
+{
+	int i;
+	printf("ENTER THE NUMBER \n");
+	scanf("%d",&i);
+	for(i=1;i<=11;i++)
+	{
+		printf("value of i=%d\n",i);
+	}
+	return 0;
+} */
+
+		// SUM OF FIRST N NATURAL NUMBERS
+	#include<stdio.h>
+	int main()
+	{
+		int i,num,sum=0;
+		printf("ENTER THE NUMBER \n");
+		scanf("%d",&num);
+		for(i=1;i<=num;i++)
+		{
+			sum=sum+i;	
+		}	
+		printf("sum=%d",sum);
+		return 0;
+	}
+	
+	
+	
+		
